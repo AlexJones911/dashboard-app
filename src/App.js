06ea9eb0ee9bf0c254d5fcf8683766e2de1395dc
ErikN42 <134044059+ErikN42@users.noreply.github.com>
2023-12-03 20:20:05 +0700

    aider: Added the necessary JavaScript code to the `src/App.js` file.
    # Aider chat conversation:
    
    ASSISTANT: To add the `src/App.js` file, we will create a new file and add the necessary JavaScript code.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/App.js
    ```javascript
    <<<<<<< SEARCH
    =======
    import React, { useState, useEffect } from 'react';
    import axios from 'axios';
    import UserProfile from './UserProfile';
    import Chatbot from './Chatbot';
    import MessageResponder from './MessageResponder';
    import './MessageResponder.css';
    import './UserDropdown.css';
    import './App.css';
    import './UserProfile.css';
    
    const App = () => {
      const [users, setUsers] = useState([{ name: 'Loading...', id: 0 }]);
      const [selectedUser, setSelectedUser] = useState(null);
    
      useEffect(() => {
        const fetchUsers = async () => {
          try {
            const response = await axios.post('http://127.0.0.1:3001/get_usernames');
            setUsers(response.data);
          } catch (error) {
            console.error('Error fetching users:', error);
            setUsers([]);
          }
        };
    
        fetchUsers();
      }, []);
    
      const UserDropdown = () => {
        const handleSelectUser = (event) => {
          const userId = event.target.value;
          setSelectedUser(userId);
        };
    
        const handleRefresh = () => {
          // Refresh functionality here
        };
    
        return (
          <div>
            <select onChange={handleSelectUser}>
              {users.map(user => (
                <option key={user.id} value={user.id}>{user.name}</option>
              ))}
            </select>
            <button onClick={handleRefresh}>Refresh</button>
          </div>
        );
      };
    
      return (
        <div className="App">
          <div className="layout">
            <div className="profile-section">
              <UserDropdown />
              {selectedUser && <UserProfile userId={selectedUser} />}
            </div>
            <div className="chatbot-section">
              <Chatbot />
            </div>
            <div className="message-responder-section">
              <MessageResponder />
            </div>
          </div>
        </div>
      );
    };
    
    export default App;
    >>>>>>> REPLACE
    ```
    
    Please replace the placeholder content with the actual content you want to use in the `App` component.

--- src/App.js
+++ src/App.js
@@ -67,3 +67,72 @@ const App = () => {
 };
 
 export default App;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import UserProfile from './UserProfile';
+import Chatbot from './Chatbot';
+import MessageResponder from './MessageResponder';
+import './MessageResponder.css';
+import './UserDropdown.css';
+import './App.css';
+import './UserProfile.css';
+
+const App = () => {
+  const [users, setUsers] = useState([{ name: 'Loading...', id: 0 }]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.post('http://127.0.0.1:3001/get_usernames');
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setUsers([]);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const UserDropdown = () => {
+    const handleSelectUser = (event) => {
+      const userId = event.target.value;
+      setSelectedUser(userId);
+    };
+
+    const handleRefresh = () => {
+      // Refresh functionality here
+    };
+
+    return (
+      <div>
+        <select onChange={handleSelectUser}>
+          {users.map(user => (
+            <option key={user.id} value={user.id}>{user.name}</option>
+          ))}
+        </select>
+        <button onClick={handleRefresh}>Refresh</button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="layout">
+        <div className="profile-section">
+          <UserDropdown />
+          {selectedUser && <UserProfile userId={selectedUser} />}
+        </div>
+        <div className="chatbot-section">
+          <Chatbot />
+        </div>
+        <div className="message-responder-section">
+          <MessageResponder />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
